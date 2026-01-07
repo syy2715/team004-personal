8cb26326f8dc74a39c839395bf166b928aff93b9
YujiShibu <colacolaqwe@gmail.com>
2026-01-07 19:30:16 +0900

    商品登録関連

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -9,7 +9,8 @@ class DatabaseSeeder extends Seeder
     public function run(): void
     {
         $this->call([
-            UserSeeder::class, // ← UserSeederを呼ぶだけにする
+            //UserSeeder::class, // ← UserSeederを呼ぶだけにする
+            itemsSeeder::class
         ]);
     }
 }
